bbb104e17c8e81aafd200df22460c81c36ae6e83
7mo <ahmedalharbi1999@outlook.com>
2026-07-14 09:29:08 +0300

    141

--- frontend/auth/auth.js
+++ frontend/auth/auth.js
@@ -1,3 +1,5 @@
+document.documentElement.style.setProperty('--navbar-opacity', '0');
+
 // --- 1. وظيفة إنشاء الحساب ---
 
 const signupBtn = document.getElementById('signupBtn');
@@ -106,6 +108,8 @@ function checkAuth() {
     if (navbar) navbar.classList.add('loaded');
 
     document.querySelectorAll('.auth-buttons, .user-menu').forEach(el => el.style.visibility = 'visible');
+
+    document.documentElement.style.setProperty('--navbar-opacity', '1');
 }
 
 // --- 4. تسجيل الخروج ---
